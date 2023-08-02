19ba15d853d1e5ebd362993372e92e6b4f984c4c
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-02 10:54:46 -0300

    Update oracle.dart

--- lib/dlc/oracle.dart
+++ lib/dlc/oracle.dart
@@ -1,1 +1,24 @@
+import 'package:bitcoin_flutter/bitcoin_flutter.dart';
+
+void main() {
+  // Generate private key and corresponding public key
+  ECPrivateKey privateKey = ECPrivateKey.generate();
+  ECPublicKey publicKey = privateKey.publicKey;
+
+  print('Private Key: ${privateKey.toHex()}');
+  print('Public Key: ${publicKey.toHex()}');
+
+  // Simulate an oracle providing an outcome
+  String oracleOutcome = 'Heads';
+  DateTime timestamp = DateTime.now();
+  String announcementMessage = '$oracleOutcome-$timestamp';
+
+  // Oracle signs the announcement message
+  String oraclePrivateKeyHex = 'oracle_private_key_in_hex';
+  ECPrivateKey oraclePrivateKey = ECPrivateKey.fromHex(oraclePrivateKeyHex);
+  ECSignature oracleSignature = privateKey.sign(Uint8List.fromList(announcementMessage.codeUnits));
+
+  print('Oracle Announcement: $announcementMessage');
+  print('Oracle Signature: ${oracleSignature.toHex()}');
+}
 
