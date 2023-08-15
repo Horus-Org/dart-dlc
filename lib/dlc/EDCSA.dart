f550fef349400af32bc8b631832c15c7e3f4527e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-15 09:20:07 -0300

    Update EDCSA.dart

--- lib/dlc/EDCSA.dart
+++ lib/dlc/EDCSA.dart
@@ -1,1 +1,40 @@
+import 'package:bitcoin/bitcoin.dart';
 
+class DlcTransaction {
+  BitcoinTransaction transaction;
+  // Add more properties and methods as needed
+}
+
+class DlcScript {
+  Script script;
+  // Add more properties and methods as needed
+}
+
+class DlcSignature {
+  ECDSASignature signature;
+  // Add more properties and methods as needed
+}
+Uint8List signMessage(Uint8List privateKey, Uint8List message) {
+  final keyPair = ECPair.fromPrivateKey(privateKey);
+  final signature = keyPair.sign(message);
+  return signature.signature;
+}
+
+Uint8List serializeDlcTransaction(DlcTransaction dlcTransaction) {
+  // Serialize using BitcoinTransaction methods
+}
+
+DlcTransaction deserializeDlcTransaction(Uint8List serializedData) {
+  // Deserialize using BitcoinTransaction methods
+}
+
+import 'package:dlc_library/dlc_library.dart';
+
+void main() {
+  // Example usage of your DLC library
+  final privateKey = Uint8List.fromList([/* your private key bytes */]);
+  final message = Uint8List.fromList([/* your message bytes */]);
+  final signature = signMessage(privateKey, message);
+
+  print('Signature: ${signature.toHexString()}');
+}
