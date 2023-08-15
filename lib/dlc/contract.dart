b3175a61526e5080644304cb282e240c0d28b990
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-15 09:28:24 -0300

    Create contract.dart

---
+++ lib/dlc/contract.dart
@@ -0,0 +1,60 @@
+import 'package:secp256k1/secp256k1.dart';
+import 'package:bitcoinex/bitcoinex.dart' as Bitcoinex;
+
+class Contract {
+  static const int defaultContractFlags = 0;
+
+  int totalCollateral;
+  List<Map<String, dynamic>> descriptor;
+  OracleInfo oracleInfo;
+
+  ExFactoContract({
+    required this.totalCollateral,
+    required this.descriptor,
+    required this.oracleInfo,
+  });
+
+  bool verify() {
+    bool verifyOracle = Oracle.verifyOracleInfo(oracleInfo);
+    bool verifyOutcomes = verifyContractOutcomesMatchEventOutcomes(descriptor, oracleInfo.event);
+
+    return verifyOracle && verifyOutcomes;
+  }
+
+  bool verifyContractOutcomesMatchEventOutcomes(
+      List<Map<String, dynamic>> descriptor, Event event) {
+    for (int i = 0; i < descriptor.length; i++) {
+      if (descriptor[i]['outcome'] != event.outcomes[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Other methods...
+}
+
+class ContractOffer {
+  // Other members and methods...
+}
+
+class ExFactoContractAccept {
+  // Other members and methods...
+}
+
+class ExFactoContractAcknowledge {
+  // Other members and methods...
+}
+
+class OracleInfo {
+  // Oracle related members and methods...
+}
+
+class Event {
+  List<String> outcomes = [];
+  // Other members and methods...
+}
+
+void main() {
+  // Your code here...
+}
