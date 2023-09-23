727373b11da9d62475630bb6f8c228985f4918d7
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-09-23 08:06:00 -0300

    Update data.dart

--- lib/dlc/data.dart
+++ lib/dlc/data.dart
@@ -1,1 +1,33 @@
+import 'dart:typed_data';
+import 'package:bitcoin_flutter/bitcoin_flutter.dart';
+
+void main() {
+  // Define contract parameters
+  double contractValue = 0.1; // Bitcoin
+  int oracleRValue = 12345; // Random Oracle value
+  int locktime = 100; // Blocks
+
+  // Create a P2WSH (Pay-to-Witness-Script-Hash) address
+  final contractScript = Script([OpTrue()]); // This is a simple placeholder script
+  final contractAddress = P2WSH(data: contractScript.encode());
+
+  // Create funding transaction
+  final fundingTx = TransactionBuilder();
+  fundingTx.addInput(OutPoint(BigInt.from(0), 0)); // Reference a UTXO
+  fundingTx.addOutput(TransactionOutput(
+      BigInt.from(contractValue * 100000000), contractAddress.scriptPubKey));
+
+  // Create the DLC Oracle script
+  final oracleScript = Script([BigIntX.fromInt(oracleRValue).toBytes(32), OpCSV(), OpTrue()]);
+
+  // Create a P2WSH address for the Oracle script
+  final oracleAddress = P2WSH(data: oracleScript.encode());
+
+  // Print contract details
+  print("Contract Address: ${contractAddress.toBase58()}");
+  print("Oracle Address: ${oracleAddress.toBase58()}");
+  print("Funding Transaction:");
+  print(fundingTx.build().toHex());
+  print("Locktime: $locktime");
+}
 
