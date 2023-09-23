47b5c06ae88ab166b26f206c33dba52c7f22e430
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-09-23 08:06:27 -0300

    Update exc.dart

--- lib/dlc/exc.dart
+++ lib/dlc/exc.dart
@@ -1,1 +1,46 @@
+import 'dart:typed_data';
+import 'package:bitcoin_flutter/bitcoin_flutter.dart';
+
+void main() {
+  // Define contract parameters
+  double contractValue = 0.1; // Bitcoin
+  int oracleRValue = 12345; // Oracle's R value
+  int locktime = 100; // Blocks
+
+  // Define funding transaction details (you need to obtain these from the blockchain)
+  String fundingTxHex = '...'; // Hex representation of the funding transaction
+  String fundingTxId = '...'; // ID of the funding transaction
+
+  // Create the DLC Oracle script (matches the Oracle's commitment)
+  final oracleScript = Script([BigIntX.fromInt(oracleRValue).toBytes(32), OpCSV(), OpTrue()]);
+
+  // Create a P2WSH address for the Oracle script
+  final oracleAddress = P2WSH(data: oracleScript.encode());
+
+  // Simulate Oracle's signature (in a real DLC, this would be provided by the Oracle)
+  final oracleSignature = '...'; // Replace with the actual Oracle's signature
+
+  // Create a spending transaction (transaction that spends the DLC funds)
+  final spendingTx = TransactionBuilder();
+  spendingTx.addInput(TransactionInput(Uint8List(32), 0)); // Reference the funding transaction
+  spendingTx.addOutput(TransactionOutput(
+      BigInt.from(contractValue * 100000000 - 1000), P2WPKH(PublicKey.fromHex('...')).data));
+  spendingTx.setLockTime(locktime);
+
+  // Add Oracle's signature as a witness
+  spendingTx.inputs[0].scriptWitness = ScriptWitness([Uint8List.fromList(oracleSignature)]);
+
+  // Sign the spending transaction (assuming you have the private key for your own address)
+  final wif = 'your_wif_private_key';
+  final privateKey = ECPair.fromWIF(wif);
+  spendingTx.sign(vin: 0, keyPair: privateKey);
+
+  // Print the signed spending transaction
+  final signedSpendingTxHex = spendingTx.build().toHex();
+  print("Signed Spending Transaction:");
+  print(signedSpendingTxHex);
+
+  // Broadcast the signed spending transaction to the Bitcoin network
+  // (You need to use a library like `http` to interact with a Bitcoin node for this step)
+}
 
