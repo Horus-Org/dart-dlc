3495431d1eb12d171cdbd204a8fadcf3de3be13a
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-03-08 08:23:36 -0300

    Update script.dart

--- lib/dlc/script.dart
+++ lib/dlc/script.dart
@@ -3,7 +3,7 @@ import 'package::bitcoin_library/script.dart';
 void main() {
   final privateKey = Uint8List.fromList([/* your private key bytes */]);
   final message = Uint8List.fromList([/* your message bytes */]);
-  final signature = signMessage(privateKey, message);
+  final signature = signMessage(privateKey, message);                                     final script = scriptMessage;
 
   print('Signature: ${signature.toHexString()}');
 }
