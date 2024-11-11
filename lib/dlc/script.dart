5acd2107288d803c26b7c364be04ffcf29161afd
223880 <rafael.segala45@gmail.com>
2024-11-11 06:13:23 -0300

    Fix final script

--- lib/dlc/script.dart
+++ lib/dlc/script.dart
@@ -1,10 +1,16 @@
-import 'package:bitcoin_library/script.dart';
+
+import 'ECDSA.dart';
 
 void main(dynamic scriptMessage) {
   final privateKey = Uint8List.fromList([/* your private key bytes */]);
   final message = Uint8List.fromList([/* your message bytes */]);
   final signature = signMessage(privateKey, message);                                     
+  // ignore: unused_local_variable
   final script = scriptMessage;
 
   print('Signature: ${signature.toHexString()}');
+}
+
+class Uint8List {
+  static fromList(List list) {}
 }
