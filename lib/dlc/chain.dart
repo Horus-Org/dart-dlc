cfb710f72a615a180affde92bd72ee5c18840178
223880 <rafael.segala45@gmail.com>
2024-11-18 06:50:13 -0300

    Fix chain

--- lib/dlc/chain.dart
+++ lib/dlc/chain.dart
@@ -34,5 +34,16 @@ void main() {
 }
 
 class ExFactoChain {
-  static Uint8List chainHash(String network) {}
-}
+  static Uint8List chainHash(String network) {
+    if (network == 'mainnet') {
+      final mainnetHashHex =
+          '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f';
+      return hexToBytes(mainnetHashHex);
+    } else if (network == 'testnet') {
+      final testnetHashHex =
+          '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943';
+      return hexToBytes(testnetHashHex);
+    } else {
+      throw ArgumentError('Invalid network: $network');
+    }
+  }}
